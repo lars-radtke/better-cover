8c59b53505d9cb66b566f11d12cab3883f5ae2c4
Lars Radtke <dev@lars-radtke.com>
2026-01-25 03:58:28 +0100

    feat: add global styles for Storybook and improve debug styling

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,13 +1,16 @@
-import type { Preview } from '@storybook/react-webpack5'
+import type { Preview } from "@storybook/react-webpack5";
+import "../dist/styles.css";
+import "./styles.css";
 
 const preview: Preview = {
   parameters: {
     controls: {
       matchers: {
-       color: /(background|color)$/i,
-       date: /Date$/i,
+        color: /(background|color)$/i,
+        date: /Date$/i,
       },
     },
+    layout: "fullscreen",
   },
 };
 
