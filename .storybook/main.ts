a79f5c86bac9b31bc7c67f16df1bc11be9c6974d
Lars Radtke <dev@lars-radtke.com>
2026-01-25 02:41:02 +0100

    refactor: update Storybook configuration and improve dev script

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,12 +1,8 @@
-import type { StorybookConfig } from '@storybook/react-webpack5';
+import type { StorybookConfig } from "@storybook/react-webpack5";
 
 const config: StorybookConfig = {
-  "stories": [
-    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-webpack5-compiler-swc"
-  ],
-  "framework": "@storybook/react-webpack5"
+  stories: ["./**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  addons: ["@storybook/addon-webpack5-compiler-swc", "@storybook/addon-styling"],
+  framework: "@storybook/react-webpack5",
 };
 export default config;
